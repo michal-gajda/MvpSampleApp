77adbc4982c9d4bae84f72d7381054aae8c246e7
Michal Gajda <michal@gajda.co.uk>
2024-10-11 07:28:00 +0100

    Init

---
+++ src/Domain/Class1.cs
@@ -0,0 +1,9 @@
+﻿#if AP
+namespace Domain
+{
+    public class Class1
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
+#endif
---
+++ src/WinUI/Form1.cs
@@ -0,0 +1,23 @@
+﻿namespace WinUI
+{
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, System.EventArgs e)
+        {
+#if AP
+            var class1 = new Domain.Class1();
+
+            class1.Message = "Hello from AP";
+
+            MessageBox.Show(class1.Message);
+#endif
+        }
+    }
+}
